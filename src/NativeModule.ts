1b4c1134da776e967f1754750a55464b0dd1fee3
Jake Teton-Landis <just.1.jake@gmail.com>
2024-07-08 20:02:32 -0400

    make it work in theory

--- src/NativeModule.ts
+++ src/NativeModule.ts
@@ -1,2 +1,17 @@
-const NativeModule = require('../.build/Module.node')
+const buildPath = '../.build'
+const basename = 'Module.node'
+const releasePath = `${process.arch}-apple-macosx/release`
+
+function requireNativeModule() {
+  try {
+    return require(`${buildPath}/${basename}`)
+  } catch (error) {
+    if ((error as any).code === 'MODULE_NOT_FOUND') {
+      return require(`${buildPath}/${releasePath}/${basename}`)
+    }
+    throw error
+  }
+}
+
+const NativeModule = requireNativeModule()
 export { NativeModule }
